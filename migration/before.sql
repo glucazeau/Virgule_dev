c2c9ae8accda364d252c8ec8dcb4f28f341ec99a
Guillaume Lucazeau <glucazeau@gmail.com>
2013-12-04 23:31:08 +0100

    Remove text from previous commit

--- migration/before.sql
+++ migration/before.sql
@@ -6,13 +6,3 @@ INSERT INTO organization_branch (id, name, address, president_name, phone_number
 INSERT INTO classroom (id, fk_organization_branch, name, comments, address) VALUES (1, '1', 'Salle de cours', 'Indisponible le mardi pour cause de réunion', '40 rue Albert Thomas 75010 Paris');
 INSERT INTO classroom (id, fk_organization_branch, name, comments, address) VALUES (2, '1', 'Musée', '', '40 rue Albert Thomas 75010 Paris');
 INSERT INTO classroom (id, fk_organization_branch, name, comments, address) VALUES (3, '1', 'Baby Boutique', '', '53 rue de Paradis 75010 Paris');
-
-| Field          | Type         | Null | Key | Default | Extra          |
-+----------------+--------------+------+-----+---------+----------------+
-| id             | int(11)      | NO   | PRI | NULL    | auto_increment |
-| name           | varchar(100) | NO   |     | NULL    |                |
-| address        | varchar(250) | YES  |     | NULL    |                |
-| president_name | varchar(100) | NO   |     | NULL    |                |
-| phone_number   | varchar(10)  | YES  |     | NULL    |                |
-| fax_number     | varchar(10)  | YES  |     | NULL    |                |
-| email_address  | varchar(45)  | YES  |     | NULL    |             
