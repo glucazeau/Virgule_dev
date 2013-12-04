3da11f763f5c055fb17c215c36db0b5d208efa98
Guillaume Lucazeau <glucazeau@gmail.com>
2013-12-05 00:34:37 +0100

    Update teachers email adresses with dummy values when empty

--- migration/after.sql
+++ migration/after.sql
@@ -7,5 +7,14 @@ INSERT INTO teacher_course (course_id, teacher_id) VALUES (124, 76);
 
 DELETE FROM comment WHERE comment = '';
 
+UPDATE teacher set phone_number = "" WHERE phone_number = "0";
+UPDATE teacher set cellphone_number = "" WHERE cellphone_number = "0";
+UPDATE student set phone_number = "" WHERE phone_number = "0";
+UPDATE student set cellphone_number = "" WHERE cellphone_number = "0";
+
+UPDATE teacher SET (username_canonical, email_canonical, enabled, expired, locked, credentials_expired) VALUES (username, email, 0, 1, 1, 1); 
+UPDATE teacher SET (enabled, expired, locked, credentials_expired) VALUES (1, 0, 0, 0) WHERE id IN (6, 73, 50, 75, 67, 76, 45, 72, 71, 51, 63, 53, 52, 68, 62, 74, 5, 64, 77, 70, 1);
+
+
 #DELETE FROM course WHERE id = '122';
 SET foreign_key_checks = 1; 
