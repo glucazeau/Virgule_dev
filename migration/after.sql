a4ef3a708fbb9ce0aae432f84c254db9f30ac7d0
Guillaume Lucazeau <glucazeau@gmail.com>
2013-12-08 01:56:26 +0100

    Fix after/before scripts

--- migration/after.sql
+++ migration/after.sql
@@ -1,7 +1,8 @@
 UPDATE teacher SET password = 'password';
+UPDATE course c set fk_semester = '13' WHERE c.id IN (118, 119, 121, 129);
+
 # Update classroom for the second class found with same day and hours
-UPDATE course c set fk_class_room = '2' WHERE c.id IN (67, 84, 87, 93, 95, 102, 109, 111, 129, 124, 126, 128, 131);
-UPDATE course c set alternate_enddate = '2013-09-01' WHERE c.id IN (118, 119, 121, 129);
+UPDATE course c set fk_class_room = '2' WHERE c.id IN (67, 84, 87, 93, 95, 102, 109, 111, 129, 124, 126, 128, 131, 133);
 
 INSERT INTO teacher_course (course_id, teacher_id) VALUES (124, 76);
 
--- migration/before.sql
+++ migration/before.sql
@@ -1,7 +1,7 @@
 
 SET foreign_key_checks = 0;
 
-UPDATE teacher SET id = 0 WHERE username = 'root_new';
+UPDATE teacher SET id = 0 WHERE username = 'root';
 INSERT INTO organization_branch (id, name, address, president_name, phone_number, fax_number, email_address) VALUES (1, 'Délégation locale de Paris III et X', '40 rue Albert Thomas 75010 Paris', 'Ludovic Tessier', '0142067905', '0142067915', 'dl.paris10@croix-rouge.fr');
 INSERT INTO classroom (id, fk_organization_branch, name, comments, address) VALUES (1, '1', 'Salle de cours', 'Indisponible le mardi pour cause de réunion', '40 rue Albert Thomas 75010 Paris');
 INSERT INTO classroom (id, fk_organization_branch, name, comments, address) VALUES (2, '1', 'Musée', '', '40 rue Albert Thomas 75010 Paris');
